d5c5fdb25b7fa0a7298fdc1a748cc71b8d21e1cd
Luca Allievi <luca@gr4vy.com>
2023-03-28 18:28:16 +0200

    Move eslint configuration and ignore to separate files

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native-community', 'prettier'],
+  env: {
+    jest: true,
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        quoteProps: 'consistent',
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'es5',
+        useTabs: false,
+      },
+    ],
+  },
+};
